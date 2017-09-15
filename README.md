67c1ac90bee270d2d42f6580b5161d8470f2bc93
Giuseppe Biolo <giuseppe.biolo@gmail.com>
2017-09-15 19:53:10 +0200

    Corrections to example menu configuration file and revision of the license references.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## shell-menu is a simple menu for Unix-like systems (Linux/HP-UX/SunOS/etc)
+## shell-menu is a simplified menu for shell environment
 
 
 ### Description
@@ -54,6 +54,19 @@ Giuseppe Biolo - giuseppe (dot) biolo (at) gmail (dot) com
 
 ### License
 
-This software is licensed under MIT license.
+This software is licensed under GPL v3 license.
 
 Copyright (c) 2017 Giuseppe Biolo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You can read the full text of the GNU General Public License version 3
+[here](http://www.gnu.org/licenses/).
--- src/shell-menu.py
+++ src/shell-menu.py
@@ -1,7 +1,6 @@
-#! /users/opscng/data03/stat/Esercizio_Pd/Python-3.6.2/python
 # /usr/bin/env python3
 
-''' shell-menu is a simple menu for Unix-like systems (Linux/HP-UX/SunOS/etc).
+''' shell-menu is a simplified menu for shell environment.
 
 Description:
     The main target of the project is to provide an easy to deploy menu to use in
@@ -37,25 +36,21 @@ Author:
     Giuseppe Biolo  < giuseppe.biolo@gmail.com > < https://github.com/gbiolo >
 
 License:
-    Copyright (c) 2017 Giuseppe Biolo
+    shell-menu  < a simplified menu for shell environment >
+    Copyright (C) 2017  Giuseppe Biolo
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
 
